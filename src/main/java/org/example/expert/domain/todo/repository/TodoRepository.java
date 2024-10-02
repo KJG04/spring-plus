9005158e4dd025990b2fb9170623f3fdf6f555ad
KJG04 <freedom7113@gmail.com>
2024-10-02 21:51:15 +0900

    feat: todo 조회 조건 변경
    5. 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE COALESCE(t.weather, 1) LIKE " +
+            "CASE " +
+            "   WHEN :weather IS NULL " +
+            "   THEN '%' " +
+            "   ELSE :weather " +
+            "END " +
+            "AND (:startAt IS NULL OR t.modifiedAt >= :startAt) " +
+            "AND (:endAt IS NULL OR t.modifiedAt <= :endAt) " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findTodosByWeatherAndModifiedAtBetween(@Param("weather") String weather, @Param("startAt") LocalDateTime startAt, @Param("endAt") LocalDateTime endAt, Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
